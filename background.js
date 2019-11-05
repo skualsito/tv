5907e66dd22be9592b2d7a0d0e8162087c664e59
Juan Almada <skual@live.com>
2019-11-05 14:05:21 -0300

    todo

--- background.js
+++ background.js
@@ -1,7 +1,8 @@
-var socket;
+var socket = io('http://localhost:8080');
+var tabloca;   
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.create({'url': chrome.extension.getURL('index.html')}, function(tab) {
-    socket = io('http://localhost:8080');
+
   });
 });
 chrome.tabs.onUpdated.addListener(function(tabid, changeInfo, tab) {
@@ -10,49 +11,50 @@ chrome.tabs.onUpdated.addListener(function(tabid, changeInfo, tab) {
 
 
     } else if(tab.url.indexOf("youtube.com") !== -1){
-      const tabloca = tabid;    
-      socket.on('mandar-funcion', function(data){
-        console.log("entra");
-        chrome.tabs.executeScript(tabloca, {file:'js/jquery.min.js'}, function(result){
-          chrome.tabs.executeScript(tabloca, {code: data}, function (){
-            console.log(data);
-          });
-        });
-      });
-
+      tabloca = tabid;   
       obtRecom();
-      socket.on('obtener-recomendados-yt', function(){
-          obtRecom();
-      });
-      function obtRecom(){
-        chrome.tabs.executeScript(tabloca, {file:'js/jquery.min.js'}, function(result){
-          chrome.tabs.executeScript(tabloca, {file:'apps/yt/home.js'}, function(resultado){
-              socket.emit("yt-on", resultado);
-          });
-        });
-      }
-      
-
       if(tab.url.indexOf("youtube.com/results") !== -1){
         obtResul();
-        socket.on('obtener-resultados-yt', function(){
-            obtResul();
-        });
-        
-        function obtResul() {
-          chrome.tabs.executeScript(tabloca, {file:'js/jquery.min.js'}, function(result){
-            chrome.tabs.executeScript(tabloca, {file:'apps/yt/busqueda.js'}, function(resultado){
-              socket.emit('servidor-resultados', resultado);
-            });
-          });
-        }
-        
-        
       }
-      
       
     }
   }
 });
 
 
+ 
+socket.on('mandar-funcion', function(data){
+  chrome.tabs.executeScript(tabloca, {file:'js/jquery.min.js'}, function(result){
+    chrome.tabs.executeScript(tabloca, {code: data}, function (){
+    });
+  });
+});
+
+
+socket.on('obtener-recomendados-yt', function(){
+    obtRecom();
+});
+function obtRecom(){
+  chrome.tabs.executeScript(tabloca, {file:'js/jquery.min.js'}, function(result){
+    chrome.tabs.executeScript(tabloca, {file:'apps/yt/home.js'}, function(resultado){
+        socket.emit("yt-on", resultado);
+    });
+  });
+}
+
+
+
+  
+socket.on('obtener-resultados-yt', function(){
+    obtResul();
+});
+
+function obtResul() {
+  chrome.tabs.executeScript(tabloca, {file:'js/jquery.min.js'}, function(result){
+    chrome.tabs.executeScript(tabloca, {file:'apps/yt/busqueda.js'}, function(resultado){
+      socket.emit('servidor-resultados', resultado);
+    });
+  });
+}
+
+  
